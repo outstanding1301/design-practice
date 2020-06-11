7ea34f7ee81ed2410284bb6c17bd82d852a545d2
outstandingboy <outstandingboy@kakao.com>
2020-06-11 11:42:36 +0900

    Updates

---
+++ precache-manifest.63e86b2fddb7a8cd94736feb9f88d8fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4af5efc7ce806b4b85eccdb06c141d3",
+    "url": "/design-practice/index.html"
+  },
+  {
+    "revision": "7bd92925b3702ede805f",
+    "url": "/design-practice/static/css/main.cab712b2.chunk.css"
+  },
+  {
+    "revision": "1c076e86794818600736",
+    "url": "/design-practice/static/js/2.3d1e78ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/design-practice/static/js/2.3d1e78ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd92925b3702ede805f",
+    "url": "/design-practice/static/js/main.18aa4d0e.chunk.js"
+  },
+  {
+    "revision": "532e00e4e2b37bb9092f",
+    "url": "/design-practice/static/js/runtime-main.8d6e6bf7.js"
+  }
+]);
